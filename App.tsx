4032feb2131f44595cdf19839433012ab01e2436
geonix-admin <geonix_official@geonix.co.kr>
2026-03-09 16:18:55 +0900

    apply reCAPTCHA to App and Contact form

--- App.tsx
+++ App.tsx
@@ -15,7 +15,7 @@ function App() {
   return (
     <LanguageProvider>
       {/* 1. 여기에 발급받은 사이트 키(6L로 시작)를 따옴표 안에 넣어주세요 */}
-      <GoogleReCaptchaProvider reCaptchaKey="6Le4WYQsAAAAAMBOULn-D87Lf1HYhxT1su-pno22"></GoogleReCaptchaProvider>
+      <GoogleReCaptchaProvider reCaptchaKey="6Le4WYQsAAAAAMBOULn-D87Lf1HYhxT1su-pno22">
       <div className="min-h-screen bg-zinc-950 text-zinc-100 font-sans selection:bg-white selection:text-black">
         <Navbar />
         <main>
